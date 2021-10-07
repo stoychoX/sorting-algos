051845c47c9bd442408cf9c37f93f09f637c404f
Stoycho Kyosev <68247567+stoychoX@users.noreply.github.com>
2021-10-07 15:55:56 +0300

    Create insertionSort.cpp

---
+++ insertionSort.cpp
@@ -0,0 +1,20 @@
+//7.10.2021
+template<class T>
+void insertionSort(T* arr, int length) {
+	for (int i = 1; i < length; i++) {
+		T lastElem = arr[i];
+		int toInsert = i;
+		for (int j = i - 1; j >= 0; --j) {
+			if (arr[j] > lastElem) {
+				arr[j + 1] = arr[j];
+				--toInsert;
+			}
+			else {
+				arr[toInsert] = lastElem;
+				break;
+			}
+		}
+		if (toInsert != i)
+			arr[toInsert] = lastElem;
+	}
+}

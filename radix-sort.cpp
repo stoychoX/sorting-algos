1a689953c672b3195f26b21b93f8bf41f0986200
Stoycho Kyosev <68247567+stoychoX@users.noreply.github.com>
2021-10-07 15:57:50 +0300

    Create radix-sort.cpp

---
+++ radix-sort.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<queue>
+
+//working only with unsigned int.
+
+void radixSort(int* arr, int len) {
+	std::queue<int> holders[10];
+
+	int digitLocation = 1;
+	bool running = true;
+
+	do {
+		int leftShift = 1;
+		int rightShift = 1;
+
+		for (int i = 0; i < digitLocation; i++)
+			leftShift *= 10;
+
+		for (int i = 1; i < digitLocation; i++)
+			rightShift *= 10;
+
+		for (int i = 0; i < len; i++) {
+			int currDigit = (arr[i] % leftShift) / rightShift;
+			holders[currDigit].push(arr[i]);
+		}
+
+		int indexForReturningTheItems = 0;
+
+		for (int i = 0; i < 10; i++) {
+			while (holders[i].empty() == false) {
+				int lastElem = holders[i].front();
+				holders[i].pop();
+				arr[indexForReturningTheItems++] = lastElem;
+			}
+		}
+
+		bool allZeroes = true;
+		for (int i = 0; i < len && allZeroes; i++)
+			allZeroes = (((arr[i] / leftShift) == 0) && allZeroes);
+
+		running = !allZeroes;
+
+		digitLocation++;
+
+	} while (running);
+}
